69d4c424de6de0632eff26cbffd5833397a96fc1
Francescosommellaa <franc.sommella@gmail.com>
2024-08-24 19:25:17 +0200

    creato setup base per il todolist

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      tasks: [],
+      newTask: ''
+    };
+  }
+}
 </script>
 
 <template>
-
+  <div>
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <span :class="{ completed: task.completed }">{{ task.text }}</span>
+        <button @click="removeTask(index)">X</button>
+      </li>
+    </ul>
+    <input type="text" v-model="newTask" placeholder="Inserisci elemento...">
+    <input type="submit" @click="addTask" value="Inserisci">
+  </div>
 </template>
 
 <style scoped>
+.completed {
+  text-decoration: line-through;
+}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      'vue': 'vue/dist/vue.esm-browser.js'
+      '/server': 'http://localhost:8000'
     }
   }
 });
